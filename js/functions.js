80f26df76f0638253b25d776231a71a001e131fa
open768 <open768@yahoo.co.uk>
2014-02-08 17:56:37 +0000

    browser validation added

--- js/functions.js
+++ js/functions.js
@@ -17,9 +17,32 @@ function clicklist(){
 
 //***************************************************************
 function reload(){
+	var sUrl, sSol, sInstrument;
+	var oList, oRadio;
+	
+	//mutex
 	if (loading) return;
+	
+	// TODO Validate that a list item and instrument are selected
+	// otherwise do nothing
+	oList = document.getElementById(SOLS_LIST);
+	sSol=oList.value;
+	if (!sSol){ 	
+		debug_console("No Sol Selected")
+		return;
+	}
+		
+	sInstrument = getRadioButtonValue(INSTRUMENT_RADIO);
+	if (!sInstrument){
+		debug_console("No instrument Selected")
+		return;
+	}
+	
+	//go ahead and get the data starting at position 0
 	loading=true;
-
+	sUrl = "php/images.php?sol=" + sSol + "&instr=" + sInstrument +"&index=0";
+	RGraph.AJAX.getJSON(sUrl, load_images_callback);
+	
 	debug_console("reloading");
 }
 
@@ -55,9 +78,14 @@ function load_instruments_callback(paJS){
 	sHTML = "";
 	for (iIndex = 0; iIndex < paJS.length; iIndex++){
 		oInstr = paJS[iIndex];
-		sHTML += "<input type='radio' name='" + INSTRUMENT_RADIO + "' value='" + oInstr.value + "' onchange='reload()'>" + oInstr.caption + "</input><br>";
+		sHTML += "<input type='radio' id='"+ INSTRUMENT_RADIO +"' name='" + INSTRUMENT_RADIO + "' value='" + oInstr.name + "' onchange='reload()'>" + oInstr.caption + "</input><br>";
 	}
 	oDiv.innerHTML = sHTML;
+	loading=false;
+}
+
+//***************************************************************
+function load_images_callback(paJS){
 	loading=false;
 }
 
@@ -68,6 +96,27 @@ function load_instruments_callback(paJS){
 function debug_console(psMessage){
 	if (DEBUG_ON && console) console.log("DEBUG> " + psMessage);
 }
+
+//***************************************************************
 function write_console(psMessage){
 	if (console) console.log(psMessage);
 }
+
+//***************************************************************
+function getRadioButtonValue(psID){
+	var oRadios = document.getElementsByName(psID);
+	var sValue = null;
+	var oRadio;
+	
+	for (var i = 0; i<oRadios.length; i++){
+		oRadio = oRadios[i];
+		if (oRadio.checked) {
+			debug_console("found a checked radio");
+			sValue = oRadio.value;
+			break;
+		}
+	}
+		
+	return sValue;
+}
+
