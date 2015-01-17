c72d8f1591c2068dd7197a084036836d2da31e2e
open768 <open768@yahoo.co.uk>
2015-01-17 09:50:12 +0000

    deteted test

--- php/test/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-	require '../inc/secret.php';
-	
-
-	$sRedirect = urlencode("http://www.mars-browser.co.uk/curiosityb/php/test/callback.php");
-	
-	header("Location: https://www.facebook.com/dialog/oauth?client_id=".cSecret::FB_APP."&redirect_uri=$sRedirect");
-?>
