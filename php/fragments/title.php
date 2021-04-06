c773236c2b77628b2542a288ced5df1a3b03fb4a
open seven six ate <open768@yahoo.co.uk>
2021-04-06 21:21:28 +0100

    updated fb login button

--- php/fragments/title.php
+++ php/fragments/title.php
@@ -1,11 +1,17 @@
+<?php
+	include("php/fragments/analytics.php");
+	include("php/fragments/facebook.php");
+?>
 <div class="titlebar">
 	<table width="100%"><tr>
 		<td style="vertical-align:middle;text-align:left">
 			<span style="align:left"><font class="title">Curiosity Browser: <span id="toptitle"><?=$sTitle?></span><?=(cHeader::is_localhost()?" - <font color='red'>DEVELOPMENT</font>":"")?></font></span>
 		</td>
 		<td style="vertical-align:middle;text-align:right">
-			<span class="FBloginButton" ID="username">Checking Login..</span>
-			<button ID="FBloginButton" class="FBloginButton" style="display:none" onclick="cFacebook.onClickLogin();">Login with FB</button>
+			<fb:login-button 
+			  scope="public_profile,email"
+			  onlogin="OnFBCheckLoginState();">
+			</fb:login-button>
 		</td>
 	</tr></table>
 </div>
