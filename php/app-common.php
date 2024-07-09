b7f4e17b2555494a10823dbce1c94bb81f4b195b
sunil <cluck@chickenkatsu.co.uk>
2024-07-09 08:34:33 +0100

    moved config to a separate file

--- php/app-common.php
+++ php/app-common.php
@@ -1,49 +1,38 @@
 <?php
-	//**********************************************************************************
-	$root=realpath($home);
-	$AppJS = "$home/js";
-	$AppJSWidgets = "$AppJS/widgets";
-	$jsinc = "$home/../jsinc";			//check this works
-	$jsExtra = "$jsinc/extra";
-	$phpinc = "$root/../phpinc";		//have to set phpinc here to pull in header.php
-	$spaceinc = "$phpinc/space";
-	//**********************************************************************************
-	
-    require_once "$phpinc/ckinc/header.php";	//this starts the session
-	require_once("$phpinc/ckinc/debug.php");
-	
-	//check for extensions
-	if (!extension_loaded("curl"))
-		cDebug::error("curl extension is not loaded - check ".php_ini_loaded_file());
-	if (!extension_loaded("sqlite3")) 
-		cDebug::error("sqlite3 extension is not loaded - check ".php_ini_loaded_file());
-	
-	//check if debugging is needed
-	cDebug::check_GET_or_POST();
+//**********************************************************************************
+include "$home/php/app-config.php";    //config for the application
+require_once "$phpinc/ckinc/header.php";    //this starts the session
+require_once("$phpinc/ckinc/debug.php");
 
-	//requests without https get redirected
-	if (!cDebug::is_cli())
-		if ($_SERVER["REQUEST_SCHEME"] !== "https"){
-			cDebug::extra_debug("request scheme is not https");
-			$https_port = "";
-			if (cDebug::is_localhost())$https_port = ":8443";
-			
-			$newURL="https://".$_SERVER["SERVER_NAME"].$https_port.$_SERVER["REQUEST_URI"];
-			cHeader::redirect($newURL);
-			exit();
-		}else{
-			cDebug::extra_debug("request scheme is https");		
-		}	
-	
-	//includes
-	require_once "$root/php/secret.php";
+//check for extensions
+if (!extension_loaded("curl"))
+    cDebug::error("curl extension is not loaded - check " . php_ini_loaded_file());
+if (!extension_loaded("sqlite3"))
+    cDebug::error("sqlite3 extension is not loaded - check " . php_ini_loaded_file());
 
-	require_once "$phpinc/ckinc/common.php";
-	require_once "$phpinc/ckinc/auth.php" ;
-	require_once "$spaceinc/curiosity/curiosity.php" ;
-	require_once "$spaceinc/curiosity/static.php" ;
+//check if debugging is needed
+cDebug::check_GET_or_POST();
 
+//requests without https get redirected
+if (!cDebug::is_cli())
+    if ($_SERVER["REQUEST_SCHEME"] !== "https") {
+        cDebug::extra_debug("request scheme is not https");
+        $https_port = "";
+        if (cDebug::is_localhost()) $https_port = ":8443";
 
+        $newURL = "https://" . $_SERVER["SERVER_NAME"] . $https_port . $_SERVER["REQUEST_URI"];
+        cHeader::redirect($newURL);
+        exit();
+    } else {
+        cDebug::extra_debug("request scheme is https");
+    }
 
-	cDebug::extra_debug("finished common.php");		
+//includes
+require_once "$root/php/secret.php";
+require_once "$phpinc/ckinc/common.php";
+require_once "$phpinc/ckinc/auth.php";
+require_once "$spaceinc/curiosity/curiosity.php";
+require_once "$spaceinc/curiosity/static.php";
+
+cDebug::extra_debug("finished app-common.php");
 ?>
---
+++ php/app-config.php
@@ -0,0 +1,9 @@
+<?php
+	$root=realpath($home);
+	$AppJS = "$home/js";
+	$AppJSWidgets = "$AppJS/widgets";
+	$jsinc = "$home/../jsinc";			//check this works
+	$jsExtra = "$jsinc/extra";
+	$phpinc = "$root/../phpinc";		//have to set phpinc here to pull in header.php
+	$spaceinc = "$phpinc/space";
+?>
