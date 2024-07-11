b924fd4c10b53fd0a24577f043777210597dbabf
sunil <cluck@chickenkatsu.co.uk>
2024-07-11 15:42:22 +0100

    working on add new relic monitoring #16

--- php/fragments/doctype.php
+++ php/fragments/doctype.php
@@ -1,4 +1,5 @@
 <?php
-    header( "Access-Control-Allow-Headers: newrelic, traceparent, tracestate")
+header("Access-Control-Allow-Headers: newrelic, traceparent, tracestate");
+header("Access-Control-Allow-Origin: *");
 ?>
 <!DOCTYPE html>
