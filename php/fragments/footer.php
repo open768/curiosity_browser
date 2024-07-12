794be6448426cbab23041fb83e7942f34ddd20ca
sunil <cluck@chickenkatsu.co.uk>
2024-07-12 13:44:34 +0100

    done use w3css in page footer #43

--- php/fragments/footer.php
+++ php/fragments/footer.php
@@ -1,27 +1,22 @@
 <p>
-<div class="github"><center>
-	<table><tr>
-		<td><div class="about">
-			<p class="credits">Data courtesy MSSS/MSL/NASA/JPL-Caltech.<br>Please note <a href="index.php#disclaim">site disclaimer</a></p>
-			<?php if (isset($sExtraCredits)){?>
-				<p class="credits"><?=$sExtraCredits?>.</p>
-			<?php }?>
-		</div></td>
-		<td><div class="about">
-			<table border=0><tr>
-				<td><img src="<?=$home?>/images/browser/chicken_icon.png"></td>
-				<td>
-					We're on <img src="<?=$home?>/images/github_logo.png"><br>
-					<a href="https://github.com/open768/curiosity_browser">https://github.com/open768/curiosity_browser</a>
-				</td>
-			</tr></table>
-		</div></td>
-		<td><div class="about">
-			<a href="https://www.facebook.com/mars.features/"><img src="<?=$home?>/images/facebook/FB-FindUsonFacebook-online-114.png" border="0"></a>
-		</div></td>
-		<td><div class="about">
-			<div class="fb-like" data-href="<?=$_SERVER['REQUEST_URI']?>" data-layout="standard" data-action="like" data-size="small" data-show-faces="false" data-share="true"></div><br>
-			<div class="fb-follow" data-href="https://www.facebook.com/mars.features/" data-layout="standard" data-size="small" data-show-faces="true"></div>
-		</div></td>
-	</tr></table>
-</center></div>
+<div class="w3-cell-row w3-light-grey w3-cell-middle">
+    <div class="w3-cell about w3-padding-small">
+        <p class="credits">Data courtesy MSSS/MSL/NASA/JPL-Caltech.<br>Please note <a href="index.php#disclaim">site disclaimer</a></p>
+        <?php if (isset($sExtraCredits)) { ?>
+            <p class="credits"><?= $sExtraCredits ?>.</p>
+        <?php } ?>
+    </div>
+    <div class="w3-cell about w3-sand w3-cell-middle">
+        <img src="<?= $home ?>/images/browser/chicken_icon.png" align="left">
+
+        We're on <img src="<?= $home ?>/images/github_logo.png"><br>
+        <a href="https://github.com/open768/curiosity_browser">https://github.com/open768/curiosity_browser</a>
+    </div>
+    <div class="w3-cell about w3-cell-middle">
+        <a href="https://www.facebook.com/mars.features/"><img src="<?= $home ?>/images/facebook/FB-FindUsonFacebook-online-114.png" border="0"></a>
+    </div>
+    <div class="w3-cell about w3-cell-middle">
+        <div class="fb-like" data-href="<?= $_SERVER['REQUEST_URI'] ?>" data-layout="standard" data-action="like" data-size="small" data-show-faces="false" data-share="true"></div><br>
+        <div class="fb-follow" data-href="https://www.facebook.com/mars.features/" data-layout="standard" data-size="small" data-show-faces="true"></div>
+    </div>
+</div>
