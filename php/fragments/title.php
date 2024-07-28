78199d39c74b21134eb04f82e4c840b5b63f01ee
sunil <cluck@chickenkatsu.co.uk>
2024-07-28 13:27:53 +0100

    additional changes to #51

--- php/fragments/title.php
+++ php/fragments/title.php
@@ -6,10 +6,10 @@ include("$appPhpFragments/google.php");
 
 <!-- Title Bar -->
 <div class="w3-cell-row w3-header-theme">
-    <div class="w3-cell title w3-text-white">
+    <div class="w3-cell">
         Curiosity Browser:
         <span id="toptitle"><?= $sTitle ?></span>
-        <?= (cDebug::is_localhost() ? " - <font color='red'>DEVELOPMENT</font>" : "") ?>
+        <?= (cDebug::is_localhost() ? " - <span><font color='red'>DEVELOPMENT</font></span>" : "") ?>
     </div>
     <div class="w3-cell w3-right-align">
         <span style="display: inline-block; width: 300px;" id="<?= cAppConfig::FB_ELEMENT_ID ?>">One Moment please...</span>
