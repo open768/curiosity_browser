c9ee6f595995b884a1e07c9b7165084326e57523
sunil <cluck@chickenkatsu.co.uk>
2024-08-06 22:17:24 +0100

    uses events to talk with cFacebook jsinc

--- php/fragments/header.php
+++ php/fragments/header.php
@@ -59,11 +59,15 @@ $sFBUser = cFacebook_ServerSide::getSessionUser();
 <script src="<?= $jsInc ?>/ck-inc/facebook.js"></script>
 
 <script>
-    cFacebook.ServerSide = "<?= $home ?>/php/rest/facebook.php";
-    cFacebook.ServerUser = "<?= $sFBUser ?>";
-    cFacebook.Version = "<?= cAppConfig::FB_VERSION ?>";
-    cFacebook.AppID = <?= $oFBAppId->id ?>;
-    bean.on(cFacebook, cFacebook.STATUS_EVENT, (psText) => $("#<?= cAppConfig::FB_ELEMENT_ID ?>").html(psText))
+    cFacebook.ServerSide = "<?= $home ?>/php/rest/facebook.php"
+    cFacebook.ServerUser = "<?= $sFBUser ?>"
+    cFacebook.Version = "<?= cAppConfig::FB_VERSION ?>"
+    cFacebook.AppID = <?= $oFBAppId->id ?>
+    bean.on(
+        cFacebook,
+        cFacebook.STATUS_EVENT,
+        (psText) => $("#<?= cAppConfig::FB_ELEMENT_ID ?>").html(psText)
+    )
 </script>
 
 <!-- New relic -->
