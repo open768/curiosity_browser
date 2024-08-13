1d05a6f2fe1124edff59c32090fdb5f334d7fc5a
sunil <cluck@chickenkatsu.co.uk>
2024-08-13 12:15:13 +0100

    #72 fixed

--- php/admin/admin_fns.php
+++ php/admin/admin_fns.php
@@ -9,7 +9,6 @@ class cAdminfunctions {
         return ($sFileName === "[iHighlite].txt");
     }
 
-    //************************************************************
     static function delete_ihighlite_files() {
         cDebug::enter();
         cPageOutput::prevent_buffering();
@@ -38,11 +37,18 @@ class cAdminfunctions {
         cDebug::leave();
     }
 
+    //************************************************************
     /**
      * adds to index comments from files on disk
      * 
      * @return never 
      */
+    static function pr_comment_filter(SplFileInfo $poFile) {
+        if ($poFile->isDir()) return true;      //allows recursion
+        $sFileName = $poFile->getFileName();
+        return ($sFileName === "[comment].txt");
+    }
+
     static function indexComments() {
         cDebug::enter();
 
@@ -50,8 +56,35 @@ class cAdminfunctions {
         $sFolder = realpath(cObjStore::$rootFolder);
         cDebug::write("scanning  folder {$sFolder} for comments files");
 
-        //
-        cDebug::error("not implemented");
+        $oIter = cCommonFiles::get_directory_iterator(
+            $sFolder,
+            function ($poFile) {
+                return self::pr_comment_filter($poFile);
+            }
+        );
+        /** @var SplFileInfo $oFile */
+        foreach ($oIter as $oFile) {
+            //-------- find the sol, instrument and product
+            $oParent = $oFile->getPathInfo();
+            $sProduct = $oParent->getBasename();
+
+            $oParent = $oParent->getPathInfo();
+            $sInstr = $oParent->getBasename();
+
+            $oParent = $oParent->getPathInfo();
+            $sSol = $oParent->getBasename();
+
+            if (!is_numeric($sSol)) continue;
+            if (!is_numeric($sInstr)) continue;
+            cDebug::write("s:{$sSol} i:{$sInstr} p:{$sProduct}");
+
+            // --------- migrate the comment itself (can only do this once so do it right)
+            $aComments = cSpaceComments::get($sSol, $sInstr, $sProduct);
+            foreach ($aComments as $oComment)
+                //---------- add comment to index (can only do this once so do it right)
+                cSpaceComments::add_to_index($sSol, $sInstr, $sProduct);
+        }
+
         cDebug::leave();
     }
 }
