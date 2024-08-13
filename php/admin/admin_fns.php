378e3f9d64d713409abefd3a958252aea2def3ff
sunil <cluck@chickenkatsu.co.uk>
2024-08-13 22:05:57 +0100

    refactored code

--- php/admin/admin_fns.php
+++ php/admin/admin_fns.php
@@ -1,53 +1,96 @@
 <?php
 
+class cMigrateHighlights {
+    //******************************************************
+    static function migrate_highlights() {
+        cDebug::enter();
+        cDebug::write("migrating Highlights");
 
-class cAdminfunctions {
-    //************************************************************
-    static function pr_ihigh_filter(SplFileInfo $poFile) {
-        if ($poFile->isDir()) return true;      //allows recursion
-        $sFileName = $poFile->getFileName();
-        return ($sFileName === "[iHighlite].txt");
+        //update the state of the migration
+        cMigrateObjdata::set_phase(cMigrateObjdata::PHASE_HIGHLIGHTS);
+
+        //get the list
+        $aList = cSpaceImageHighlight::get_top_index();
+
+        //iterate through the list
+        foreach ($aList as $iSol => $iCount)
+            if ($iSol > cMigrateObjdata::$LastSol) {
+                $aProducts = cSpaceImageHighlight::get_all_highlights($iSol);
+                cMigrateObjdata::set_last_sol($iSol);
+                cDebug::write("migrated highlights for sol $iSol");
+            }
+
+        //next migration
+        cMigrateObjdata::set_last_sol(cMigrateObjdata::BEFORE_MIGRATION_SOL);
+        cMigrateTags::migrate_product_tags();
+        cDebug::leave();
     }
 
-    static function delete_ihighlite_files() {
+    //******************************************************
+    static function pr_mosaic_filter(SplFileInfo $poFile) {
+        if ($poFile->isDir()) return true;      //allows recursion
+        $sFileName = $poFile->getFileName();
+        return ($sFileName === "[moscount].txt");
+    }
+
+    //******************************************************
+    static function migrate_mosaics() {
         cDebug::enter();
+        cDebug::write("migrating mosaics");
 
-        //find all files named "[iHighlite].txt"
+        //update the state of the migration
         $sFolder = realpath(cObjStore::$rootFolder);
-        cDebug::extra_debug("looking in $sFolder");
-
-        //************************************************************
+        cDebug::write("directory is $sFolder");
+        cMigrateObjdata::set_phase(cMigrateObjdata::PHASE_MOSAIC);
         $oIter = cCommonFiles::get_directory_iterator(
             $sFolder,
-            function (SplFileInfo $po) {
-                return self::pr_ihigh_filter($po);
+            function ($poFileInfo) {
+                return self::pr_mosaic_filter($poFileInfo);
             }
         );
-
         /** @var SplFileInfo $oFile */
-        $oFile = null;
-        cDebug::extra_debug("starting directory walk");
-        foreach ($oIter as $oFile) {
-            $sPath = $oFile->getpathname();
-            cDebug::write("deleting $sPath");
-            @unlink($sPath);
+        foreach ($oIter as  $oFile) {
+            $sPath = $oFile->getPath();
+            $oParentInfo = $oFile->getPathInfo();
+            $sSol = $oParentInfo->getBasename();
+            cDebug::write("found file: $sPath");
+            cSpaceImageHighlight::get_mosaic_sol_highlight_count($sSol);
         }
+
+        //next step
+        cMigrateObjdata::set_last_sol(cMigrateObjdata::BEFORE_MIGRATION_SOL);
+        cMigrateObjdata::set_phase(cMigrateObjdata::PHASE_COMPLETE);
+        cDebug::leave();
+    }
+}
+
+//##########################################################################
+class cMigrateComments {
+    //******************************************************
+    static function migrate_comments() {
+        cDebug::enter();
+        cDebug::write("migrating comments");
+
+        //update the state of the migration
+        cMigrateObjdata::set_phase(cMigrateObjdata::PHASE_COMMENTS);
+        //get the list
+        self::indexComments();
+
+        //next migration
+        cMigrateObjdata::set_last_sol(cMigrateObjdata::BEFORE_MIGRATION_SOL);
+        cMigrateHighlights::migrate_mosaics();
 
         cDebug::leave();
     }
 
-    //************************************************************
-    /**
-     * adds to index comments from files on disk
-     * 
-     * @return never 
-     */
+    //******************************************************
     static function pr_comment_filter(SplFileInfo $poFile) {
         if ($poFile->isDir()) return true;      //allows recursion
         $sFileName = $poFile->getFileName();
         return ($sFileName === "[comment].txt");
     }
 
+    //******************************************************
     static function indexComments() {
         cDebug::enter();
 
@@ -86,45 +129,158 @@ class cAdminfunctions {
 
         cDebug::leave();
     }
-    //************************************************************
-    /**
-     * adds to index comments from files on disk
-     * 
-     * @return never 
-     */
-    static function pr_mosaic_filter(SplFileInfo $poFile) {
-        if ($poFile->isDir()) return true;      //allows recursion
-        $sFileName = $poFile->getFileName();
-        return ($sFileName === "[moscount].txt");
-    }
+}
 
-    //******************************************************
-    static function migrate_mosaics() {
+//##########################################################################
+class cMigrateGigas {
+    static function migrate_gigas() {
         cDebug::enter();
-        cDebug::write("migrating mosaics");
+        cDebug::write("migrating Gigapans");
 
         //update the state of the migration
-        $sFolder = realpath(cObjStore::$rootFolder);
-        cDebug::write("directory is $sFolder");
-        cMigrateObjdata::set_phase(cMigrateObjdata::PHASE_MOSAIC);
+        cMigrateObjdata::set_phase(cMigrateObjdata::PHASE_GIGA);
+
+        //get the list
+        $aList = cPencilNev::get_top_gigas();
+
+        //iterate through the list
+        foreach ($aList as $iSol => $iCount)
+            if ($iSol > cMigrateObjdata::$LastSol) {
+                $aProducts = cPencilNev::get_sol_gigas($iSol);
+                cMigrateObjdata::set_last_sol($iSol);
+                cDebug::write("done gigas for sol $iSol");
+                cDebug::flush();
+            }
+
+        //next migration
+        cMigrateObjdata::set_last_sol(cMigrateObjdata::BEFORE_MIGRATION_SOL);
+        cMigrateComments::migrate_comments();
+
+        cDebug::leave();
+    }
+}
+
+//##########################################################################
+class cMigrateTags {
+    //******************************************************
+    static function pr_filter_product_tag(SplFileInfo $poFile) {
+        if ($poFile->isDir()) return true;      //allows recursion
+        $sFileName = $poFile->getFileName();
+        return ($sFileName === cSpaceTags::PRODUCT_TAG_FILE);
+    }
+
+    static function mop_up_product_tags() {
         $oIter = cCommonFiles::get_directory_iterator(
-            $sFolder,
+            cObjStore::$rootFolder,
             function ($poFileInfo) {
-                return self::pr_mosaic_filter($poFileInfo);
+                return self::pr_filter_product_tag($poFileInfo);
             }
         );
         /** @var SplFileInfo $oFile */
-        foreach ($oIter as  $oFile) {
-            $sPath = $oFile->getPath();
-            $oParentInfo = $oFile->getPathInfo();
-            $sSol = $oParentInfo->getBasename();
-            cDebug::write("found file: $sPath");
-            cSpaceImageHighlight::get_mosaic_sol_highlight_count($sSol);
+        foreach ($oIter as $oFile) {
+            $sPath = $oFile->getPathname();
+            cDebug::write("found $sPath");
+            cDebug::error("stop");
+        }
+    }
+
+    static function migrate_product_tags() {
+        cDebug::enter();
+        cDebug::write("migrating product Tags");
+
+        //update the state of the migration
+        cMigrateObjdata::set_phase(cMigrateObjdata::PHASE_PRODUCT_TAGS);
+
+        //get the list
+        $aList = cSpaceTags::get_top_sol_index();
+
+        //iterate through the list
+        foreach ($aList as $iSol => $iCount)
+            if ($iSol > cMigrateObjdata::$LastSol) {
+                $aInstrData = cSpaceTags::get_sol_tags($iSol);
+                if (is_array($aInstrData)) {
+                    foreach ($aInstrData as $sInstr => $aTags) {
+                        cSpaceTags::get_instr_index($iSol, $sInstr);
+                        foreach ($aTags as $oItem) {
+                            $sProduct = $oItem["p"];
+                            cSpaceTags::get_product_tags($iSol, $sInstr, $sProduct);
+                        }
+                    }
+                }
+
+                cMigrateObjdata::set_last_sol($iSol);
+                cDebug::write("done  sol $iSol");
+            }
+
+        //mop up tag files
+        self::mop_up_product_tags();
+
+        //next migration
+        cMigrateObjdata::set_last_sol(cMigrateObjdata::BEFORE_MIGRATION_SOL);
+        cMigrateTags::migrate_tag_names();
+
+        cDebug::leave();
+    }
+
+
+    //***************************************************************
+    static function migrate_tag_names() {
+        cDebug::enter();
+
+        //update the state of the migration
+        cDebug::write("migrating Tag names");
+        cMigrateObjdata::set_phase(cMigrateObjdata::PHASE_TAG_NAMES);
+
+        //
+        $aTags = cSpaceTags::get_top_tag_names();
+
+        foreach ($aTags as $sTag => $sCount) {
+            if ($sTag === "") continue;
+            cDebug::write("Tag is $sTag");
+            cSpaceTags::get_tag_name_index($sTag);
         }
 
-        //next step
+        //next migration
         cMigrateObjdata::set_last_sol(cMigrateObjdata::BEFORE_MIGRATION_SOL);
-        cMigrateObjdata::set_phase(cMigrateObjdata::PHASE_COMPLETE);
+        cMigrateGigas::migrate_gigas();
+
+        cDebug::leave();
+    }
+}
+
+//##########################################################################
+class cAdminfunctions {
+    //************************************************************
+    static function pr_ihigh_filter(SplFileInfo $poFile) {
+        if ($poFile->isDir()) return true;      //allows recursion
+        $sFileName = $poFile->getFileName();
+        return ($sFileName === "[iHighlite].txt");
+    }
+
+    static function delete_ihighlite_files() {
+        cDebug::enter();
+
+        //find all files named "[iHighlite].txt"
+        $sFolder = realpath(cObjStore::$rootFolder);
+        cDebug::extra_debug("looking in $sFolder");
+
+        //************************************************************
+        $oIter = cCommonFiles::get_directory_iterator(
+            $sFolder,
+            function (SplFileInfo $po) {
+                return self::pr_ihigh_filter($po);
+            }
+        );
+
+        /** @var SplFileInfo $oFile */
+        $oFile = null;
+        cDebug::extra_debug("starting directory walk");
+        foreach ($oIter as $oFile) {
+            $sPath = $oFile->getpathname();
+            cDebug::write("deleting $sPath");
+            @unlink($sPath);
+        }
+
         cDebug::leave();
     }
 }
--- php/admin/migrate_objdata.php
+++ php/admin/migrate_objdata.php
@@ -53,6 +53,8 @@ class cMigrateObjdata {
     static function migrate() {
         cDebug::enter();
         $sPhase = self::initialise();
+        cMigrateTags::mop_up_product_tags();
+        cDebug::error("stop");
 
         cDebug::extra_debug("migration phase is '$sPhase'");
 
@@ -60,31 +62,30 @@ class cMigrateObjdata {
         switch ($sPhase) {
             case null:
                 // migration not attempted
-                self::pr_migrate_highlights();
                 break;
             case self::PHASE_HIGHLIGHTS:
                 cDebug::write("completing highlight migration");
-                self::pr_migrate_highlights();
+                cMigrateHighlights::migrate_highlights();
                 break;
             case self::PHASE_TAG_NAMES:
                 cDebug::write("completing tag name migration");
-                self::pr_migrate_tag_names();
+                cMigrateTags::migrate_tag_names();
                 break;
             case self::PHASE_PRODUCT_TAGS:
                 cDebug::write("completing product tag migration");
-                self::pr_migrate_product_tags();
+                cMigrateTags::migrate_product_tags();
                 break;
             case self::PHASE_COMMENTS:
                 cDebug::write("completing comment migration");
-                self::pr_migrate_comments();
+                cMigrateComments::migrate_comments();
                 break;
             case self::PHASE_GIGA:
                 cDebug::write("completing gigapan migration");
-                self::pr_migrate_gigas();
+                cMigrateGigas::migrate_gigas();
                 break;
             case self::PHASE_MOSAIC:
                 cDebug::write("completing mosaic migration");
-                cAdminfunctions::migrate_mosaics();
+                cMigrateHighlights::migrate_mosaics();
                 break;
             case self::PHASE_COMPLETE:
                 // migration completed
@@ -113,138 +114,6 @@ class cMigrateObjdata {
 
         //update the state of the migration
         $oDB->put(self::MIGRATED_PHASE, $psPhase);
-    }
-
-    //*******************************************************************
-    //*
-    //*******************************************************************
-    private static function pr_migrate_highlights() {
-        cDebug::enter();
-        cDebug::write("migrating Highlights");
-
-        //update the state of the migration
-        self::set_phase(self::PHASE_HIGHLIGHTS);
-
-        //get the list
-        $aList = cSpaceImageHighlight::get_top_index();
-
-        //iterate through the list
-        foreach ($aList as $iSol => $iCount)
-            if ($iSol > self::$LastSol) {
-                $aProducts = cSpaceImageHighlight::get_all_highlights($iSol);
-                self::set_last_sol($iSol);
-                cDebug::write("migrated highlights for sol $iSol");
-            }
-
-        //next migration
-        self::set_last_sol(self::BEFORE_MIGRATION_SOL);
-        self::pr_migrate_product_tags();
-        cDebug::leave();
-    }
-
-
-    //******************************************************
-    private static function pr_migrate_comments() {
-        cDebug::enter();
-        cDebug::write("migrating comments");
-
-        //update the state of the migration
-        self::set_phase(self::PHASE_COMMENTS);
-        //get the list
-        cAdminfunctions::indexComments();
-
-        //next migration
-        self::set_last_sol(self::BEFORE_MIGRATION_SOL);
-        cAdminfunctions::migrate_mosaics();
-
-        cDebug::leave();
-    }
-
-    //******************************************************
-    private static function pr_migrate_tag_names() {
-        cDebug::enter();
-
-        //update the state of the migration
-        cDebug::write("migrating Tag names");
-        self::set_phase(self::PHASE_TAG_NAMES);
-
-        //
-        $aTags = cSpaceTags::get_top_tag_names();
-
-        foreach ($aTags as $sTag => $sCount) {
-            if ($sTag === "") continue;
-            cDebug::write("Tag is $sTag");
-            cSpaceTags::get_tag_name_index($sTag);
-        }
-
-        //next migration
-        self::set_last_sol(self::BEFORE_MIGRATION_SOL);
-        self::pr_migrate_gigas();
-
-        cDebug::leave();
-    }
-
-    //******************************************************
-    private static function pr_migrate_product_tags() {
-        cDebug::enter();
-        cDebug::write("migrating product Tags");
-
-        //update the state of the migration
-        self::set_phase(self::PHASE_PRODUCT_TAGS);
-
-        //get the list
-        $aList = cSpaceTags::get_top_sol_index();
-
-        //iterate through the list
-        foreach ($aList as $iSol => $iCount)
-            if ($iSol > self::$LastSol) {
-                $aInstrData = cSpaceTags::get_sol_tags($iSol);
-                if (is_array($aInstrData)) {
-                    foreach ($aInstrData as $sInstr => $aTags) {
-                        cSpaceTags::get_instr_index($iSol, $sInstr);
-                        foreach ($aTags as $oItem) {
-                            $sProduct = $oItem["p"];
-                            cSpaceTags::get_product_tags($iSol, $sInstr, $sProduct);
-                        }
-                    }
-                }
-
-                self::set_last_sol($iSol);
-                cDebug::write("done  sol $iSol");
-            }
-
-        //next migration
-        self::set_last_sol(self::BEFORE_MIGRATION_SOL);
-        self::pr_migrate_tag_names();
-
-        cDebug::leave();
-    }
-
-    //******************************************************
-    private static function pr_migrate_gigas() {
-        cDebug::enter();
-        cDebug::write("migrating Gigapans");
-
-        //update the state of the migration
-        self::set_phase(self::PHASE_GIGA);
-
-        //get the list
-        $aList = cPencilNev::get_top_gigas();
-
-        //iterate through the list
-        foreach ($aList as $iSol => $iCount)
-            if ($iSol > self::$LastSol) {
-                $aProducts = cPencilNev::get_sol_gigas($iSol);
-                self::set_last_sol($iSol);
-                cDebug::write("done gigas for sol $iSol");
-                cDebug::flush();
-            }
-
-        //next migration
-        self::set_last_sol(self::BEFORE_MIGRATION_SOL);
-        self::pr_migrate_comments();
-
-        cDebug::leave();
     }
 }
 
