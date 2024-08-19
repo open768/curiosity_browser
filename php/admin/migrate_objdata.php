d038669f861ad59593b6c6ef465251b92ca57a28
sunil <cluck@chickenkatsu.co.uk>
2024-08-19 15:34:21 +0100

    removed some debug statements

--- php/admin/migrate_objdata.php
+++ php/admin/migrate_objdata.php
@@ -25,11 +25,9 @@ class cMigrateObjdata {
 
     //*******************************************************************
     static function init_obj_store_db() {
-        cDebug::enter();
         if (self::$objstoreDB == null) {
             self::$objstoreDB = new cObjStoreDB(self::OBJDB_REALM, "migrate");
         }
-        cDebug::leave();
     }
 
 
