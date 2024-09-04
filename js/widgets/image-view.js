d72acb9e22205c7eba6b42dc01ba9344d69d13a8
sunil <cluck@chickenkatsu.co.uk>
2024-09-04 14:50:25 +0100

    #98 and #123

--- js/widgets/image-view.js
+++ js/widgets/image-view.js
@@ -1,18 +1,232 @@
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 // % Definition
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-$.widget('ck.imageview', {
+class cImageList {
+    IMAGES_URL = cAppLocations.rest + '/images.php';
+    IMAGES_TO_SHOW = 5;
+    CURRENT_CHILD_ID = 'CILC';
+
+    widget = null;
+    element = null;
+    options = null;
+    start_image = 1;
+
+    //********************************************************
+    constructor(poWidget) {
+        this.widget = poWidget;
+        this.element = poWidget.element;
+        this.options = poWidget.options;
+
+        //------------------checks
+        const oOptions = this.options;
+        if (oOptions.sol == null) $.error('sol is not set');
+        if (oOptions.instrument == null) $.error('instrument is not set');
+        if (oOptions.mission == null) $.error('mission is not set');
+    }
+
+    //*****************************************************************************
+    load_images(piStartImage) {
+        const oOptions = this.options;
+        this.widget._trigger('onStatus', null, { text: 'loading Images' });
+
+        const oThis = this;
+        const oElement = this.element;
+
+        //------------------- add a spinner
+        oElement.empty();
+
+        var sCaption =
+            'Loading Images for sol:' +
+            oOptions.sol +
+            ' instr:' +
+            oOptions.instrument;
+        const oDiv = cAppRender.make_spinner(sCaption);
+        oElement.append(oDiv);
+
+        // --------------update the Browser url
+        var oParams = {};
+        oParams[cSpaceBrowser.BEGIN_QUERYSTRING] = piStartImage;
+        oParams[cSpaceBrowser.SOL_QUERYSTRING] = oOptions.sol;
+        oParams[cSpaceBrowser.INSTR_QUERYSTRING] = oOptions.instrument;
+        var sUrl = cBrowser.buildUrl(cBrowser.pageUrl(), oParams);
+        cBrowser.update_state('index', sUrl);
+
+        //update the navigation bar current page
+
+        // ------------------load images
+        sUrl = cBrowser.buildUrl(this.IMAGES_URL, {
+            s: oOptions.sol,
+            i: oOptions.instrument,
+            b: piStartImage,
+            e: piStartImage + this.IMAGES_TO_SHOW,
+            m: oOptions.mission.ID,
+        });
+        const oHttp = new cHttp2();
+        {
+            bean.on(oHttp, 'result', (poHttp) => oThis.onImagesLoaded(poHttp));
+            oHttp.fetch_json(sUrl);
+        }
+    }
+
+    //*****************************************************************************
+    pr_render_navigation(piMax) {
+        const oElement = this.element;
+        const oOptions = this.options;
+        const oThis = this;
+        oElement.empty();
+
+        var oDiv = $('<div>', {
+            class: 'W3-container w3-theme-d3',
+        });
+        {
+            //-----------------------------------------------------------------------
+            var bDisabled = oOptions.start_image < this.IMAGES_TO_SHOW;
+            var oPrevPageButton = cAppRender.make_button(
+                null,
+                '<< Previous',
+                'previous Page',
+                bDisabled,
+                () => oThis.onClickPreviousPage(),
+            );
+            {
+                oPrevPageButton.width('200px');
+                oDiv.append(oPrevPageButton);
+            }
+
+            //-----------------------------------------------------------------------
+            var sID = cJquery.child_ID(oElement, this.CURRENT_CHILD_ID);
+            var oCurrentDiv = $('<span>', {
+                id: sID,
+                class: 'w3-margin-left w3-margin-right',
+            });
+            {
+                oCurrentDiv.append(oOptions.start_image);
+                oDiv.append(oCurrentDiv);
+            }
+
+            //-----------------------------------------------------------------------
+            bDisabled = oOptions.start_image >= piMax - this.IMAGES_TO_SHOW;
+            var oNextPageButton = cAppRender.make_button(
+                null,
+                'Next >>',
+                'next Page',
+                bDisabled,
+                () => oThis.onClickNextPage(),
+            );
+            {
+                oNextPageButton.width('200px');
+                oDiv.append(oNextPageButton);
+            }
+
+            //-----------------------------------------------------------------------
+            oDiv.append(' Max:' + piMax);
+        }
+        oElement.append(oDiv);
+    }
+
+    //*****************************************************************************
+    //* Events
+    //*****************************************************************************
+    onImagesLoaded(poHttp) {
+        const oThis = this;
+        var oDiv, iIndex;
+
+        this.widget._trigger('onStatus', null, { text: 'showing images' });
+        const oJson = poHttp.response;
+        const oOptions = this.options;
+
+        // clear out the image div
+        this.element.empty();
+
+        // build the html to put into the div
+        if (oJson.max == 0) {
+            this.element.append(cAppRender.make_note('No data found'));
+        } else {
+            // nothing in this div
+            oOptions.max_images = oJson.max;
+            oOptions.start_image = parseInt(oJson.start);
+
+            this.pr_render_navigation(oJson.max);
+            // create the navigation div
+
+            // display the images
+            for (iIndex = 0; iIndex < oJson.images.length; iIndex++) {
+                // get the img details
+                let oItem = oJson.images[iIndex];
+
+                // build up the div
+                oDiv = $('<DIV>').instrumentimage({
+                    sol: oOptions.sol,
+                    instrument: oOptions.instrument,
+                    product: oItem.p,
+                    mission: oOptions.mission,
+                    src: oItem.i,
+                    date: oItem.du,
+                    onClick: function (poEvent, poData) {
+                        oThis.widget._trigger('onClick', poEvent, poData);
+                    },
+                    onStatus: function (poEvent, paData) {
+                        oThis.widget._trigger('onStatus', poEvent, paData);
+                    },
+                });
+                this.element.append(oDiv);
+                this.element.append('<P>');
+            }
+        }
+
+        // set up keypress
+        if (!oOptions.keypress_done) {
+            this.widget._on(window, {
+                keypress: function (poEvent) {
+                    oThis.onKeypress(poEvent);
+                },
+            });
+            oOptions.keypress_done = true;
+        }
+
+        this.widget._trigger('onLoaded', null, oOptions.start_image);
+    }
+
+    //**************************************************************************************************
+    onClickPreviousPage() {
+        const oOptions = this.options;
+        if (oOptions.start_image < this.IMAGES_TO_SHOW) return;
+        this.load_images(oOptions.start_image - this.IMAGES_TO_SHOW);
+    }
+
+    //**************************************************************************************************
+    onClickNextPage() {
+        const oOptions = this.options;
+        if (oOptions.start_image >= oOptions.max_images - this.IMAGES_TO_SHOW)
+            return;
+
+        this.load_images(oOptions.start_image + this.IMAGES_TO_SHOW);
+    }
+
+    //**************************************************************************************************
+    onKeypress(poEvent) {
+        if (poEvent.target.tagName === 'INPUT') return;
+
+        const sChar = String.fromCharCode(poEvent.which);
+
+        switch (sChar) {
+            case 'n':
+                this.onClickNextPage();
+                break;
+            case 'p':
+                this.onClickPreviousPage();
+                break;
+        }
+    }
+}
+
+//################################################################################
+//#
+//################################################################################
+$.widget('ck.image_list', {
     //#################################################################
     //# Definition
     //#################################################################
-    consts: {
-        IMAGES_URL: cAppLocations.rest + '/images.php',
-        IMAGES_TO_SHOW: 5,
-        CurrentID: 'C',
-        LeftID: 'L',
-        RightID: 'R',
-        MaxID: 'M',
-    },
     options: {
         sol: null,
         instrument: null,
@@ -26,8 +240,6 @@ $.widget('ck.imageview', {
     //# Constructor
     //#################################################################
     _create: function () {
-        // check that the element is a div
-
         // check for necessary classes
         if (!bean) {
             $.error('bean class is missing! check includes');
@@ -52,190 +264,7 @@ $.widget('ck.imageview', {
         if (oOptions.mission == null) $.error('mission is not set');
 
         // start the images load
-        this._trigger('onStatus', null, { text: 'loading Images' });
-        this._load_images(oOptions.start_image);
-    },
-
-    //#################################################################
-    //# Methods
-    //#################################################################
-    _load_images: function (piStartImage) {
-        const oThis = this;
-        const oOptions = this.options;
-
-        // put some info in the widget
-        const oElement = this.element;
-        oElement.empty();
-
-        var sCaption =
-            'Loading Images for sol:' +
-            oOptions.sol +
-            ' instr:' +
-            oOptions.instrument;
-        const oDiv = cAppRender.make_spinner(sCaption);
-        oElement.append(oDiv);
-
-        // load images
-        const sUrl = cBrowser.buildUrl(this.consts.IMAGES_URL, {
-            s: oOptions.sol,
-            i: oOptions.instrument,
-            b: piStartImage,
-            e: piStartImage + this.consts.IMAGES_TO_SHOW,
-            m: oOptions.mission.ID,
-        });
-        const oHttp = new cHttp2();
-        {
-            bean.on(oHttp, 'result', (poHttp) => oThis.onFullImages(poHttp));
-            oHttp.fetch_json(sUrl);
-        }
-    },
-
-    //#################################################################
-    //# events
-    //#################################################################
-    onKeypress: function (poEvent) {
-        if (poEvent.target.tagName === 'INPUT') return;
-
-        const sChar = String.fromCharCode(poEvent.which);
-
-        switch (sChar) {
-            case 'n':
-                this.onClickNextPage();
-                break;
-            case 'p':
-                this.onClickPreviousPage();
-                break;
-        }
-    },
-
-    //**************************************************************************************************
-    onFullImages: function (poHttp) {
-        const oThis = this;
-        var oDiv, iIndex;
-
-        this._trigger('onStatus', null, { text: 'showing images' });
-        const oJson = poHttp.response;
-        const oOptions = this.options;
-
-        // clear out the image div
-        this.element.empty();
-
-        // build the html to put into the div
-        if (oJson.max == 0) {
-            this.element.append(cAppRender.make_note('No data found'));
-        } else {
-            // nothing in this div
-            this.element.empty();
-            oOptions.max_images = oJson.max;
-
-            // update title
-            oOptions.start_image = parseInt(oJson.start);
-
-            // create the navigation div
-            const sID = this.element.id;
-            const oNavTable = $('<table>', { class: 'gold', width: '100%' });
-            const oRow = $('<TR>');
-            var oCell = $('<TD>', { width: '40%' });
-            var oButton = $('<button>', {
-                class: 'leftarrow imagenav',
-                title: 'Previous Page',
-                id: sID + this.consts.LeftID,
-            });
-            if (oOptions.start_image < this.consts.IMAGES_TO_SHOW) {
-                oButton.attr('disabled', 'disabled');
-            } else {
-                oButton.html('Previous Page').click(function () {
-                    oThis.onClickPreviousPage();
-                });
-            }
-            oCell.append(oButton);
-            oRow.append(oCell);
-            oCell = $('<TD>', {
-                width: '10%',
-                id: sID + this.consts.CurrentID,
-                align: 'middle',
-            }).html(oJson.start);
-            oRow.append(oCell);
-            oCell = $('<TD>', { width: '40%' });
-            oButton = $('<button>', {
-                class: 'rightarrow imagenav',
-                title: 'Previous Page',
-                id: sID + this.consts.LeftID,
-            });
-            if (
-                oOptions.start_image >=
-                oJson.max - this.consts.IMAGES_TO_SHOW
-            ) {
-                oButton.attr('disabled', 'disabled');
-            } else {
-                oButton.html('Next Page').click(function () {
-                    oThis.onClickNextPage();
-                });
-            }
-            oCell.append(oButton);
-            oRow.append(oCell);
-            oCell = $('<TD>', { align: 'right' });
-            oCell.append(' Max:' + oJson.max);
-            oRow.append(oCell);
-            oNavTable.append(oRow);
-            this.element.append(oNavTable);
-
-            // display the images
-            for (iIndex = 0; iIndex < oJson.images.length; iIndex++) {
-                // get the img details
-                let oItem = oJson.images[iIndex];
-
-                // build up the div
-                oDiv = $('<DIV>').instrumentimage({
-                    sol: oOptions.sol,
-                    instrument: oOptions.instrument,
-                    product: oItem.p,
-                    mission: oOptions.mission,
-                    src: oItem.i,
-                    date: oItem.du,
-                    onClick: function (poEvent, poData) {
-                        oThis._trigger('onClick', poEvent, poData);
-                    },
-                    onStatus: function (poEvent, paData) {
-                        oThis._trigger('onStatus', poEvent, paData);
-                    },
-                });
-                this.element.append(oDiv);
-                this.element.append('<P>');
-            }
-
-            // add the navigation controls again (have to clone)
-            this.element.append(oNavTable.clone(true));
-        }
-
-        // set up keypress
-        if (!oOptions.keypress_done) {
-            this._on(window, {
-                keypress: function (poEvent) {
-                    oThis.onKeypress(poEvent);
-                },
-            });
-            oOptions.keypress_done = true;
-        }
-
-        this._trigger('onLoaded', null, oOptions.start_image);
-    },
-
-    //**************************************************************************************************
-    onClickPreviousPage: function () {
-        const oOptions = this.options;
-        if (oOptions.start_image < this.consts.IMAGES_TO_SHOW) return;
-        this._load_images(oOptions.start_image - this.consts.IMAGES_TO_SHOW);
-    },
-
-    //**************************************************************************************************
-    onClickNextPage: function () {
-        const oOptions = this.options;
-        if (
-            oOptions.start_image >=
-            oOptions.max_images - this.consts.IMAGES_TO_SHOW
-        )
-            return;
-        this._load_images(oOptions.start_image + this.consts.IMAGES_TO_SHOW);
+        var oInstance = new cImageList(this);
+        oInstance.load_images(oOptions.start_image);
     },
 });
