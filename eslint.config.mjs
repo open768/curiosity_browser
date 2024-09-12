c07ec7ffa3d6ab429f57dab16bd53922c2b3452b
sunil <cluck@chickenkatsu.co.uk>
2024-09-12 12:23:15 +0100

    minor changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,42 +1,43 @@
 // eslint.config.js
-import globals from 'globals';
-import js from '@eslint/js';
+import globals from 'globals'
+import js from '@eslint/js'
 
 export default [
-    js.configs.recommended,
-    {
-        languageOptions: {
-            globals: {
-                ...globals.jquery,
-                ...globals.browser,
-                bean: 'readonly',
-                cAppLocations: 'readonly',
-                cAppRender: 'readonly',
-                cAuth: 'readonly',
-                cBrowser: 'readonly',
-                cCommon: 'readonly',
-                cCommonStatus: 'readonly',
-                cCuriosity: 'readonly',
-                cDebug: 'readonly',
-                cDetailPageConstants: 'readonly',
-                cFacebook: 'readonly',
-                cGoogleEarth: 'readonly',
-                cHttp2: 'readonly',
-                cImgHilite: 'readonly',
-                cJquery: 'readonly',
-                cMission: 'readonly',
-                cRenderGoogleFont: 'readonly',
-                cSpaceBrowser: 'readonly',
-                cSpaceComments: 'readonly',
-                cTagging: 'readonly',
-                cHttpQueue: 'readonly',
-                cHttpQueueItem: 'readonly',
-                cActionQueue: 'readonly',
-            },
-        },
-        rules: {
-            'no-unused-vars': 'warn',
-            semi: 'off',
-        },
-    },
-];
+	js.configs.recommended,
+	{
+		languageOptions: {
+			globals: {
+				...globals.jquery,
+				...globals.browser,
+				bean: 'readonly',
+				cActionQueue: 'readonly',
+				cAppLocations: 'readonly',
+				cAppRender: 'readonly',
+				cAuth: 'readonly',
+				cBrowser: 'readonly',
+				cCommon: 'readonly',
+				cCommonStatus: 'readonly',
+				cCuriosity: 'readonly',
+				cDebug: 'readonly',
+				cDetailPageConstants: 'readonly',
+				cFacebook: 'readonly',
+				cGoogleEarth: 'readonly',
+				cHttp2: 'readonly',
+				cHttpQueue: 'readonly',
+				cHttpQueueItem: 'readonly',
+				cImgHilite: 'readonly',
+				cJquery: 'readonly',
+				cMission: 'readonly',
+				cRenderGoogleFont: 'readonly',
+				cSpaceBrowser: 'readonly',
+				cSpaceComments: 'readonly',
+				cString: 'readonly',
+				cTagging: 'readonly'
+			}
+		},
+		rules: {
+			'no-unused-vars': 'warn',
+			semi: 'off'
+		}
+	}
+]
