25b9d1bfe623734f1032506ab5bfc0ed1039c3d3
sunil <cluck@chickenkatsu.co.uk>
2024-10-13 21:40:59 +0100

    added secret template

---
+++ php/app-config/app-secret-template.php
@@ -0,0 +1,34 @@
+<?php
+
+/**************************************************************************
+Copyright (C) Chicken Katsu 2014 -2015
+
+This code is protected by copyright under the terms of the 
+Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License
+http://creativecommons.org/licenses/by-nc-nd/4.0/legalcode
+
+For licenses that allow for commercial use please contact cluck@chickenkatsu.co.uk
+
+// USE AT YOUR OWN RISK - NO GUARANTEES OR ANY FORM ARE EITHER EXPRESSED OR IMPLIED
+ **************************************************************************/
+
+class cAppSecret {
+    //facebook
+    const FB_APP = "";
+    const FB_SECRET = "";
+
+    const FB_DEV_APP = "";
+    const FB_DEV_SECRET = "";
+
+    //google
+    const USE_GOOGLE_ANALYTICS = false;
+    const GOOGLE_TRACKING_ID = "";
+    const GOOGLE_ANALYTICS_ID = "";
+
+    //others
+    const NASA_OPEN_MARS_API_KEY = "";      //not used
+
+    //appdynamics
+    const USE_APPD = false;
+    const APPD_RUM_APPKEY = "";
+}
