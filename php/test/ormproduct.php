8fb931a592d3c82c1f853bb898474c91bd199e3b
sunil <cluck@chickenkatsu.co.uk>
2025-07-21 21:29:15 +0100

    calendar fixed

--- php/test/ormproduct.php
+++ php/test/ormproduct.php
@@ -1,7 +1,7 @@
 <?php
 
 /**************************************************************************
-Copyright (C) Chicken Katsu 2013 -2024
+Copyright (C) Chicken Katsu 2013 - 2024
 
 This code is protected by copyright under the terms of the 
 Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License
@@ -10,17 +10,19 @@ http://creativecommons.org/licenses/by-nc-nd/4.0/legalcode
 For licenses that allow for commercial use please contact cluck@chickenkatsu.co.uk
 
 // USE AT YOUR OWN RISK - NO GUARANTEES OR ANY FORM ARE EITHER EXPRESSED OR IMPLIED
+//
  **************************************************************************/
-
 $home = "../..";
-require_once  "$home/php/fragments/app-common.php";
+require_once "$home/php/fragments/app-common.php";
 include cAppGlobals::$appPhpFragments . "/rest_header.php";
 
-require_once  cAppGlobals::$spaceInc . "/curiosity/instrument.php";
 
-$sSol = cHeader::get(cSpaceUrlParams::SOL, true, true);
-cDebug::write($sSol);
-$aData = cMSLManifestOrmUtils::get_calendar($sSol);
 
-//############################### response ####################
-cCommon::write_json($aData);
+cDebug::extra_debug("<h2>search for product</h2>");
+$oProduct = cMSLManifestOrmUtils::search_for_product("2838ML0148470330109174C00");
+if ($oProduct == null)
+    cPageOutput::errorbox("failed test");
+else {
+    cPageOutput::messagebox("success test");
+    cDebug::vardump($oProduct);
+}
